43b74ff34831d16b784f6b738dcf401d8bc43646
Guilherme Nunes <joseguilhermeenunesarmento@gmail.com>
2026-06-24 00:28:56 -0300

    Versão 3

--- ecommerce-prata/app/main.py
+++ ecommerce-prata/app/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-
-from app.database import engine, get_db
+from fastapi import HTTPException
+from app.database import get_db, engine
 from app.models.product import Base, Product
 from app.schemas.product import ProductCreate, ProductResponse
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="E-commerce de Prata e Pedras Naturais"
+)
+
+@app.get("/")
+def home():
+    return {"mensagem": "API funcionando"}
+
 
 @app.post("/products", response_model=ProductResponse)
 def create_product(
@@ -26,3 +33,27 @@ def create_product(
     db.refresh(new_product)
 
     return new_product
+
+@app.get("/products", response_model=list[ProductResponse])
+def list_products(
+    db: Session = Depends(get_db)
+):
+    products = db.query(Product).all()
+    return products
+
+@app.get("/products/{product_id}", response_model=ProductResponse)
+def get_product(
+    product_id: int,
+    db: Session = Depends(get_db)
+):
+    product = db.query(Product).filter(
+        Product.id == product_id
+    ).first()
+
+    if not product:
+        raise HTTPException(
+            status_code=404,
+            detail="Produto não encontrado"
+        )
+
+    return product
