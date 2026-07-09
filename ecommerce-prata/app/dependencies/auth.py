3806dff894ec37a967b6ad9a9301ab6e83fde24d
Guilherme Nunes <joseguilhermeenunesarmento@gmail.com>
2026-07-09 10:43:51 -0300

    versão 13 , instalação da autenticação e da autorização camadas clientes e camadas admin

--- ecommerce-prata/app/dependencies/auth.py
+++ ecommerce-prata/app/dependencies/auth.py
@@ -1,9 +1,13 @@
-from fastapi import Depends, HTTPException
+from fastapi import Depends
+from fastapi import HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
-from app.core.database import get_db
+
 from app.core.security import decode_access_token
+from app.core.database import get_db
 from app.repositories.user_repository import UserRepository
+from fastapi import Depends, HTTPException, status
+from app.models.user import User
 
 oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl="/auth/login"
@@ -35,3 +39,13 @@ def get_current_user(
         )
 
     return user
+
+def get_current_admin(
+    current_user: User = Depends(get_current_user)
+):
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Você não tem permissão para acessar este recurso."
+        )
+    return current_user
